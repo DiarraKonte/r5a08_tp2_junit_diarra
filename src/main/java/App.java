79ce9897b5bb7ee3a900858a3dc5a63cb15c6087
Dreskine <dkonte95100@gmail.com>
2025-11-30 16:19:19 +0100

    premier test avec user strory

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -11,22 +11,38 @@ import fr.einfolearning.tp2.utilitaires.EmacsAnalyze;
 
 public class App {
 
-    public static void main(String[] args) throws EmacsKillRingOverflowException{
+    public static void main(String[] args) throws EmacsKillRingOverflowException {
         TextEditor texte = new TextEditor("Je suis un editeur de texte");
         EmacsAnalyze.dumpTextEditor(texte, true, true);
 
-
-        // Etape deux : sauvegarder "suis" dans le tampon circulaire    
+        // Etape deux : sauvegarder "suis" dans le tampon circulaire
         texte.setCursor(3);
         texte.setMark(7);
-        texte.killRingBackup();  // copier "suis"     
+        texte.killRingBackup(); // copier "suis"
+        EmacsAnalyze.dumpTextEditor(texte, true, true);
+
+        // Etape trois :couper la sous chaines "editeur"
+        texte.setCursor(11);
+        texte.setMark(16);
+        texte.killSection(); // copier "suis"
+        EmacsAnalyze.dumpTextEditor(texte, true, true);
+
+        // Etape quatre : coller "editeur" avant "un"\
+        texte.setCursor(8);
+        try {
+            texte.yank();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         EmacsAnalyze.dumpTextEditor(texte, true, true);
         
-
-        // Etape trois : supprimer "suis" dans le tampon circulaire    
-        texte.setCursor(3);
-        texte.setMark(7);
-        texte.killRingBackup();  // copier "suis"     
+        // Etape cinq : ecraser la sous chaine que l'on vient de coller avec le mot "suis"
+        try {
+            texte.yankPop();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         EmacsAnalyze.dumpTextEditor(texte, true, true);
+
     }
 }
