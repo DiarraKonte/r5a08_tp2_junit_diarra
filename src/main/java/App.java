faad9e15dee4692960c3283792b17ec4555e8d48
diarrakonte <diarrakontepro@gmail.com>
2025-11-30 14:33:58 +0100

    debut refactorisation

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -7,10 +7,26 @@ import fr.einfolearning.tp2.metiers.EmacsKillRing;
 import fr.einfolearning.tp2.metiers.TextBuffer;
 import fr.einfolearning.tp2.metiers.TextEditor;
 import fr.einfolearning.tp2.metiers.exceptions.EmacsKillRingOverflowException;
+import fr.einfolearning.tp2.utilitaires.EmacsAnalyze;
 
 public class App {
 
-    public static void main(String[] args) {
-        // A completer
+    public static void main(String[] args) throws EmacsKillRingOverflowException{
+        TextEditor texte = new TextEditor("Je suis un editeur de texte");
+        EmacsAnalyze.dumpTextEditor(texte, true, true);
+
+
+        // Etape deux : sauvegarder "suis" dans le tampon circulaire    
+        texte.setCursor(3);
+        texte.setMark(7);
+        texte.killRingBackup();  // copier "suis"     
+        EmacsAnalyze.dumpTextEditor(texte, true, true);
+        
+
+        // Etape trois : supprimer "suis" dans le tampon circulaire    
+        texte.setCursor(3);
+        texte.setMark(7);
+        texte.killRingBackup();  // copier "suis"     
+        EmacsAnalyze.dumpTextEditor(texte, true, true);
     }
 }
